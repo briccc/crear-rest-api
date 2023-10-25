b0f6ab429e1217f5de7b685da2e1c586954d6d84
Bricia <bricia.candela.diaz.01.06@gmail.com>
2023-10-25 16:25:31 -0300

    Crear libro

--- src/controller.js
+++ src/controller.js
@@ -19,8 +19,19 @@ class LibroController {
           res.json({ "Error": "No existe el libro" });
         }
       }
-      
 
+      //create one
+      async createOne(req, res) {
+        try {
+          const libro = req.body;
+          const [result] = await pool.query('INSERT INTO Libros(nombre, autor, categoria, anio_publicacion, ISBN) VALUES (?, ?, ?, ?, ?)', [libro.nombre, libro.autor, libro.categoria, libro.anio_publicacion, libro.ISBN]);
+          
+          res.json({ "Libro creado": result.insertId });
+        } catch (error) {
+          console.error(error);
+          res.status(500).json({ error: "Error al crear libro" });
+        }
+      }
 
 }
 
